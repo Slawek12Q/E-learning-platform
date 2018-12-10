0882333f50695326f2849a824c7b38524dafb89c
Slawek12Q <slawek.guzik@interia.pl>
2018-12-10 23:48:54 +0100

    initial

---
+++ project/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\lenovo\desktop\mojewa~1\django\e-lear~1\project\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
